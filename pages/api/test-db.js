7b812f00fd09914f6c912928a8f68c5a2ccb7588
Node Warrior <warriorcarl@yahoo.com>
2024-10-21 19:52:04 +0700

    Remove all files from repository

--- pages/api/test-db.js
+++
@@ -1,7 +0,0 @@
-import { connectToDatabase } from '../../lib/mongodb';
-
-export default async function handler(req, res) {
-  const { db } = await connectToDatabase();
-  const data = await db.collection('test').find({}).toArray();
-  res.status(200).json(data);
-}

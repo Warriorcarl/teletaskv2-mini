fbed563c5c7a336b738ef92a274820ff48b9a939
Node Warrior <100796465+Warriorcarl@users.noreply.github.com>
2024-10-22 00:28:07 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default {
+  env: {
+    TELEGRAM_WEB_APP_SCRIPT: 'https://telegram.org/js/telegram-web-app.js',
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+};
